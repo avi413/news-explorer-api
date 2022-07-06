de5da9e2888c57fd9419ef83d639ce7a5068e435
Avi Dalal <avi413@gmail.com>
2022-07-06 17:01:54 +0300

    init

--- middlewares/errors/unauthorized-error.js
+++
@@ -1,8 +0,0 @@
-class UnauthorizedError extends Error {
-  constructor(message) {
-    super(message);
-    this.statusCode = 401;
-    this.message = message;
-  }
-}
-module.exports = UnauthorizedError;
